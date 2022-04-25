dfbdd465a270ff96d060f7ee22d6451f02c54dbf
Lucas-COX <lucas.cox@epitech.eu>
2022-04-25 15:47:17 +0200

    feat: add file parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import Lib
+import System.Environment (getArgs)
+import Lib (closest, parseFile, Point ())
+import Text.Read (readMaybe)
+import Data.Maybe (isNothing, fromJust)
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    content <- readFile $ head args
+    case parseFile content of
+        Nothing -> print "Invalid arguments"
+        Just x -> if isNothing (readMaybe (args !! 1) :: Maybe Point)
+            then print "Invalid arguments"
+            else print (closest x (fromJust (readMaybe (args !! 1) :: Maybe Point)))
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,38 @@
 module Lib
-    ( someFunc
+    (
+        distance,
+        closest,
+        parseFile,
+        Point
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Read (readMaybe)
+import Data.Maybe (isNothing, fromJust, fromMaybe)
+
+type Point = (Double, Double, Double)
+
+
+parseFileLines :: [String] -> [Point] -> Maybe [Point]
+parseFileLines (a:as) ps = case (readMaybe a :: Maybe Point) of
+    Nothing -> Nothing
+    Just any -> parseFileLines as $ ps ++ [any]
+parseFileLines [] ps = Just ps
+
+
+parseFile :: String -> Maybe [Point]
+parseFile file = parseFileLines (lines file) []
+
+
+distance :: Point -> Point -> Double
+distance (xa, ya, za) (xb, yb, zb) = sqrt ((xb - xa) ^ 2 + (yb - ya) ^ 2 + (zb - za) ^ 2)
+
+
+findClosestPoint :: [Point] -> Point -> Maybe Point -> Maybe Point
+findClosestPoint (p:ps) p2 c = if isNothing c || distance p p2 < distance p (fromJust c)
+    then findClosestPoint ps p2 (Just p)
+    else findClosestPoint ps p2 c
+findClosestPoint [] _ c = c
+
+
+closest :: [Point] -> Point -> Point
+closest ps p2 = fromMaybe (0, 0, 0) (findClosestPoint ps p2 Nothing)
